80f61377bd19e98b88456b798ef127e810b59401
Kaaviya <Kaaviya@DESKTOP-V10I7DJ>
2020-02-16 22:18:46 -0800

    First commit

---
+++ test/src/test/java/com/automation/test/Print.java
@@ -0,0 +1,14 @@
+package com.automation.test;
+
+import org.testng.annotations.Test;
+
+public class Print {
+	
+@Test
+public void sumtest()
+	{
+		System.out.println("my name is sasi");
+
+	}
+
+}
