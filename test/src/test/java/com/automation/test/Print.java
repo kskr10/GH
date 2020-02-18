3a6fedea02008d3116b8eb27a9333deaffe51780
Kaaviya <Kaaviya@DESKTOP-V10I7DJ>
2020-02-17 22:44:02 -0800

    test1123

--- test/src/test/java/com/automation/test/Print.java
+++ test/src/test/java/com/automation/test/Print.java
@@ -8,7 +8,8 @@ public class Print {
 public void sumtest()
 	{
 		System.out.println("my name is sasi");
-		System.out.println("my name is sasi1");
+		System.out.println("my name is sasi1123");
+		
 
 	}
 
